e75965506742570a281659eb0194e0d7aa6beb53
briancollins-92 <bcollins.sw@gmail.com>
2020-12-15 07:37:06 +0000

    Added constants for directory service

--- src/main/java/com/bc92/projectsdk/constants/DirectoryServiceConstants.java
+++ src/main/java/com/bc92/projectsdk/constants/DirectoryServiceConstants.java
@@ -2,10 +2,25 @@ package com.bc92.projectsdk.constants;
 
 public final class DirectoryServiceConstants {
 
-  /** Constant representing the REGEX expression for validating file paths and names */
-  public static final String FILE_PATH_REGEX = "^[^\\\\#?$\"\'<>:;\\|\\*]*$";
-
   public static final String PATH_DELIMINATOR = "/";
+
+  /** Constant representing the REGEX expression for validating file paths and names */
+  public static final String FILE_PATH_REGEX =
+      "^" + PATH_DELIMINATOR + "[^\\\\#?$\"\'<>:;\\|\\*]*$";
+
+  /** Constant representing the REGEX expression for validating a single path discriminator */
+  public static final String DISCRIMINATOR_REGEX =
+      "^[^\\" + PATH_DELIMINATOR + "\\\\#?$\"\'<>:;\\|\\*]*$";
+
+  public static final String DIRECTORY_API_PATH = "/directory-service/directories";
+
+  public static final String FOLDER_API_PATH = "/directory-service/folders";
+
+  public static final String FILE_API_PATH = "/directory-service/files";
+
+  public static final String ROOT_NODE_NAME = "root";
+
+  public static final String ROOT_PARENT_PATH = "";
 
   private DirectoryServiceConstants() {}
 
